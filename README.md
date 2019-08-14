9273a003aca0c7906d0064591765a0e96d1751fd
Mike Benkovich <mike@benko.com>
2019-08-14 12:44:39 -0500

    Add project files.

---
+++ README.md
@@ -0,0 +1,9 @@
+# UC ITPS .NET Developers Identity Story
+
+The Microsoft Identity story has evolved over the years since the release of the Membership Provider in .NET 2.0. In this session we explore how the identity story in .NET has evolved from early days of username/password to working with integrated and 3rd party identity providers. We will talk about what's new and how to implement basic identity with the tools available to the developer.
+
+## Demos
+1. Default Identity
+2. Scaffolding and Multifactor Auth
+3. 3rd Party Identity Providers
+4. Authorization
---
+++ myAuthDemo/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using myAuthDemo.Models;
+
+namespace myAuthDemo.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [Authorize(Roles ="Admin")]
+        public IActionResult Admin()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
